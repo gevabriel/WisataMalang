09229d94480c34b9764f789c3518a1a537dd208a
Gibran Hakim <90363814+gevabriel@users.noreply.github.com>
2023-06-07 09:07:50 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -63,9 +63,6 @@
                 <?php
               } else {
                 ?>
-                <?php
-                include 'profilpicture.php';
-                ?>
 
                 <ul class="navbar-nav ms-auto ms-md 0 me-3 me-lg-4">
                   <li class="nav-item dropdown">
